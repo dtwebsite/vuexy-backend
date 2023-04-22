74fba57c927bb7ff99436b22e6dc4161e259bd48
dennis.tseng <a0981038610@gmail.com>
2023-04-23 01:55:29 +0800

    後端初版-管理員 權限及異動

---
+++ README.md
@@ -0,0 +1,9 @@
+#後端模板
+```
+composer install
+cp .env.example .env
+php artisan key:generate
+php artisan migrate:fresh --seed
+帳號 : admin@gmail.com
+密碼 : 123qweasd
+```
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longtext('avatar')->nullable()->comment('大頭貼');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
+            $table->tinyInteger('status')->default(1)->comment('狀態');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
